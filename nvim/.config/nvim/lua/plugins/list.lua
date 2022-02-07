59847173cdcadc9e2a66d235e814f3f735d28ab9
Morten Vistisen <mbv@mortenvistisen.com>
2022-02-07 14:49:44 +0100

    good setup now

--- nvim/.config/nvim/lua/plugins/list.lua
+++ nvim/.config/nvim/lua/plugins/list.lua
@@ -28,12 +28,12 @@ Plug ('fatih/vim-go', { ['do'] = ':GoUpdateBinaries' })
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-nvim-lsp'
-
--- Plug 'hrsh7th/cmp-buffer'
--- Plug 'hrsh7th/cmp-path'
--- Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
 Plug 'saadparwaiz1/cmp_luasnip'
 Plug 'L3MON4D3/LuaSnip' -- Snippets plugin
+
 -- Plug 'glepnir/lspsaga.nvim'
 
 -- treesitter
