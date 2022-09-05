2cfe7eb78ff45c009b5fbb07a8a62746162bcd09
MBvisti <mbv@mortenvistisen.com>
2022-09-05 14:52:20 +0200

    added relative path to lualine

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -16,10 +16,10 @@ lualine.setup {
     lualine_c = {{
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
-      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
     }},
     lualine_x = {
-      { 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}, },
+      { 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = 'Error', warn = 'Warn', info = 'Info', hint = 'Hint'}, },
       'encoding',
       'filetype'
     },
